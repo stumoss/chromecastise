d3c6457ac8a3d96f7c9dbca94d191fd63e67cdc4
Stuart Moss <samoss@gmail.com>
2016-01-25 22:22:40 +0000

    Multiple updates
    - Update clap to version 2
    - Adds m4v as allowed input file format
    - Default to using mp4 as output container format
    - Prevent conversion from happening when file format and codecs match

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,11 @@
 extern crate clap;
 
 use std::path::{Path, PathBuf};
-use clap::{App, Arg, ArgMatches};
+use clap::{App, Arg, ArgMatches, AppSettings};
 use std::process::Command;
 
-const KNOWN_FILE_EXTENSIONS: [&'static str; 11] = ["mkv", "avi", "mp4", "3gp", "mov", "mpg",
-                                                   "mpeg", "qt", "wmv", "m2ts", "flv"];
+const KNOWN_FILE_EXTENSIONS: [&'static str; 12] = ["mkv", "avi", "mp4", "3gp", "mov", "mpg",
+                                                   "mpeg", "qt", "wmv", "m2ts", "flv", "m4v"];
 
 const SUPPORTED_VIDEO_CODECS: [&'static str; 1] = ["AVC"];
 const SUPPORTED_AUDIO_CODECS: [&'static str; 1] = ["AAC"];
@@ -18,10 +18,11 @@ const DEFAULT_AUDIO_CODEC: &'static str = "aac";
 fn main() {
     let matches = parse_args();
 
-    let container_format = match matches.is_present("mp4") {
-        true => "mp4",
-        false => "mkv",
-    };
+    let mut container_format = "mp4";
+
+    if matches.is_present("mkv") {
+        container_format = "mkv";
+    }
 
     let file = Path::new(matches.value_of("file").unwrap());
     let test = matches.is_present("test");
@@ -46,9 +47,11 @@ fn process_file(file: &Path, container_format: &str, test: bool) {
                      .unwrap_or_else(|e| panic!("Failed to execute process: {}", e));
 
     let mut output_video_codec = DEFAULT_VIDEO_CODEC;
-    let original_video_codec = std::str::from_utf8(&output.stdout).unwrap_or_else(|e| {
-        panic!("Failed to extract video codec from output: {}", e)
-    });
+    let original_video_codec = std::str::from_utf8(&output.stdout)
+                                   .unwrap_or_else(|e| {
+                                       panic!("Failed to extract video codec from output: {}", e)
+                                   })
+                                   .trim_right();
 
     if SUPPORTED_VIDEO_CODECS.contains(&original_video_codec.trim_right()) {
         output_video_codec = "copy";
@@ -63,15 +66,18 @@ fn process_file(file: &Path, container_format: &str, test: bool) {
                      .unwrap_or_else(|e| panic!("Failed to execute process: {}", e));
 
     let mut output_audio_codec = DEFAULT_AUDIO_CODEC;
-    let original_audio_codec = std::str::from_utf8(&output.stdout).unwrap_or_else(|e| {
-        panic!("Failed to extract audio codec from output: {}", e)
-    });
+    let original_audio_codec = std::str::from_utf8(&output.stdout)
+                                   .unwrap_or_else(|e| {
+                                       panic!("Failed to extract audio codec from output: {}", e)
+                                   })
+                                   .trim_right();
 
     if SUPPORTED_AUDIO_CODECS.contains(&original_audio_codec.trim_right()) {
         output_audio_codec = "copy";
     }
 
-    if output_video_codec == "copy" && output_audio_codec == "copy" {
+    println!("Container format: {} -> {}", ext, container_format);
+    if output_video_codec == "copy" && output_audio_codec == "copy" && ext == container_format {
         println!("{:?} - No conversion required", file);
         return;
     }
@@ -83,23 +89,16 @@ fn process_file(file: &Path, container_format: &str, test: bool) {
                              file.file_stem().unwrap().to_str().unwrap(),
                              container_format));
 
-    println!("Converted file {}: Video: {} -> {} Audio: {} -> {}",
-             output_file.display(),
-             original_video_codec,
-             output_video_codec,
-             original_audio_codec,
-             output_audio_codec);
-
     if !test {
         Command::new("ffmpeg")
             .arg("-threads")
             .arg("4")
             .arg("-i")
-            .arg(file)
+            .arg(&file)
             .arg("-map")
             .arg("0:0")
             .arg("-c:v")
-            .arg(output_video_codec)
+            .arg(&output_video_codec)
             .arg("-preset")
             .arg("slow")
             .arg("-level")
@@ -115,22 +114,30 @@ fn process_file(file: &Path, container_format: &str, test: bool) {
             .arg("-map")
             .arg("0:1")
             .arg("-c:a:0")
-            .arg(output_audio_codec)
+            .arg(&output_audio_codec)
             .arg("-b:a:0")
             .arg("128k")
             .arg("-strict")
             .arg("-2")
             .arg("-y")
-            .arg(output_file)
+            .arg(&output_file)
             .status()
             .unwrap_or_else(|e| panic!("Failed to execute process: {}", e));
     }
+
+    println!("Transcoded file {}: Video: {} -> {} Audio: {} -> {}",
+             output_file.display(),
+             original_video_codec,
+             output_video_codec,
+             original_audio_codec,
+             output_audio_codec);
 }
 
-fn parse_args<'a, 'b>() -> ArgMatches<'a, 'b> {
+fn parse_args<'a>() -> ArgMatches<'a> {
     App::new("chromecastise")
         .version(&crate_version!()[..])
         .author("Stuart Moss <samoss@gmail.com>")
+        .setting(AppSettings::ArgRequiredElseHelp)
         .arg(Arg::with_name("mp4")
                  .long("mp4")
                  .short("a")
@@ -142,15 +149,15 @@ fn parse_args<'a, 'b>() -> ArgMatches<'a, 'b> {
                  .short("b")
                  .help("Use mkv as the output container format")
                  .takes_value(false))
-        .arg(Arg::with_name("file")
-                 .required(true)
-                 .index(1)
-                 .help("The file to convert"))
         .arg(Arg::with_name("test")
                  .required(false)
                  .long("test")
                  .short("t")
                  .help("Test to see if conversion is required")
                  .takes_value(false))
+        .arg(Arg::with_name("file")
+                 .required(true)
+                 .index(1)
+                 .help("The file to convert"))
         .get_matches()
 }
