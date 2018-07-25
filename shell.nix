edef78a98b22a04d46e7b391036173a9d278d00d
Stuart Moss <samoss@gmail.com>
2018-07-25 10:56:04 +0100

    Start preview of 2018 rust edition

--- shell.nix
+++ shell.nix
@@ -1,11 +1,20 @@
-with import <nixpkgs> {};
-
 let
+  moz_overlay = import (builtins.fetchTarball https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz);
+  nixpkgs = import <nixpkgs> { overlays = [ moz_overlay ]; };
   version = "1.0.3";
 in
-rustPlatform.buildRustPackage {
+
+with nixpkgs;
+
+stdenv.mkDerivation {
   name = "chromecastise-${version}";
-  buildInputs = [ rustfmt ];
   src = ./.;
+
   cargoSha256 = "1dmybqbbica7k9z9f25yzak3ji4np5mhl9xly35gjypdd2jzcf2j";
+
+  buildInputs = [
+    rustfmt
+    latest.rustChannels.nightly.rust
+    carnix
+  ];
 }
