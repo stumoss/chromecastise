626092ac17e394363c4cd0a26fd4391efec5bc03
Stuart Moss <samoss@gmail.com>
2019-03-14 14:21:22 +0000

    Add docker and nix shell files

--- default.nix
+++ default.nix
@@ -1,4 +1,4 @@
-{ stdenv, rustPlatform }:
+{ stdenv, rustPlatform, ffmpeg, mediainfo }:
 
 rustPlatform.buildRustPackage rec {
   name = "chromecastise-${version}";
@@ -7,6 +7,11 @@ rustPlatform.buildRustPackage rec {
   src = ./.;
 
   cargoSha256 = "1dmybqbbica7k9z9f25yzak3ji4np5mhl9xly35gjypdd2jzcf2j";
+
+  buildInputs = [
+    ffmpeg
+    mediainfo
+    ];
 
   meta = with stdenv.lib; {
     description = "Wrapper for ffmpeg to convert your media for use with chromecast";
