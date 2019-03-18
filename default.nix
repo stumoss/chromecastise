9382d5fc2a7328ddee7f0c5c1228ff16b0c91282
Stuart Moss <samoss@gmail.com>
2019-03-18 18:16:44 +0000

    Add shell completions

--- default.nix
+++ default.nix
@@ -13,6 +13,13 @@ rustPlatform.buildRustPackage rec {
     mediainfo
     ];
 
+   preFixup = ''
+    mkdir -p "$out/share/"{bash-completion/completions,fish/vendor_completions.d,zsh/site-functions}
+    cp target/release/build/chromecastise-*/out/chromecastise.bash "$out/share/bash-completion/completions/"
+    cp target/release/build/chromecastise-*/out/chromecastise.fish "$out/share/fish/vendor_completions.d/"
+    cp target/release/build/chromecastise-*/out/_chromecastise "$out/share/zsh/site-functions/"
+  '';
+
   meta = with stdenv.lib; {
     description = "Wrapper for ffmpeg to convert your media for use with chromecast";
     homepage = https://github.com/stumoss/chromecastise;
--- shell.nix
+++ shell.nix
@@ -3,7 +3,7 @@ with import <nixpkgs> {};
 rustPlatform.buildRustPackage rec {
   name = "chromecastise-${version}";
   version = "1.0.3";
-  buildInputs = [ mediainfo ffmpeg ];
+  buildInputs = [ mediainfo ffmpeg gitAndTools.gitflow ];
   src = ./.;
   cargoSha256 = "1dmybqbbica7k9z9f25yzak3ji4np5mhl9xly35gjypdd2jzcf2j";
 }
