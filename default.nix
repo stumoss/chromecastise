8796f46e8b6a958ddd61cacb9a2bbd813a9456eb
Stuart Moss <samoss@gmail.com>
2019-09-30 16:27:12 +0100

    Correctly add runtime dependencies

--- default.nix
+++ default.nix
@@ -1,19 +1,30 @@
-{ stdenv, rustPlatform, ffmpeg, mediainfo }:
+{ rustPlatform, fetchFromGitHub, openssl, ffmpeg-full, stdenv, makeWrapper, mediainfo }:
 
-rustPlatform.buildRustPackage rec {
+with rustPlatform;
+
+buildRustPackage rec {
   name = "chromecastise-${version}";
   version = "1.0.6";
 
-  src = ./.;
+  src = ./.; 
 
   cargoSha256 = "0lr8gn0fh4hag3ynkcjdkzckr5vyxk8c19wnn5xaz2dhpl44bd68";
 
-  buildInputs = [
-    ffmpeg
-    mediainfo
-    ];
+  nativeBuildInputs = [
+    openssl
+    makeWrapper
+  ];
 
-   preFixup = ''
+  buildInputs = [
+    ffmpeg-full
+    mediainfo
+  ];
+
+  postInstall = ''
+        wrapProgram $out/bin/chromecastise --prefix PATH : ${stdenv.lib.strings.makeBinPath [ mediainfo ffmpeg-full ]}
+  '';
+
+  preFixup = ''
     mkdir -p "$out/share/"{bash-completion/completions,fish/vendor_completions.d,zsh/site-functions}
     cp target/release/build/chromecastise-*/out/chromecastise.bash "$out/share/bash-completion/completions/"
     cp target/release/build/chromecastise-*/out/chromecastise.fish "$out/share/fish/vendor_completions.d/"
